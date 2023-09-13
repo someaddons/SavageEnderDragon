3c9406f5282c712576fb5b38310f86bf1e47b826
someaddons <someaddons@github.com>
2023-09-13 15:12:50 +0200

    4.1: Add support for entity nbt values for spawn config entries

--- src/main/java/com/dragonfight/config/ConfigurationCache.java
+++ src/main/java/com/dragonfight/config/ConfigurationCache.java
@@ -4,6 +4,8 @@ import com.dragonfight.DragonfightMod;
 import com.dragonfight.fight.DragonFightManagerCustom;
 import com.google.common.collect.ImmutableList;
 import net.minecraft.core.Registry;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.TagParser;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 
@@ -18,15 +20,21 @@ public class ConfigurationCache
         DragonFightManagerCustom.spawnOnDragonSitting = parseEntityTypes(DragonfightMod.config.getCommonConfig().spawnwhilelanded);
     }
 
-    private static ImmutableList<EntityType> parseEntityTypes(final List<String> data)
+    private static ImmutableList<EntitySpawnData> parseEntityTypes(final List<String> data)
     {
-        final ImmutableList.Builder<EntityType> builder = ImmutableList.builder();
+        final ImmutableList.Builder<EntitySpawnData> builder = ImmutableList.builder();
         for (final String entry : data)
         {
             final String[] splitEntry = entry.split(",");
             for (final String entityString : splitEntry)
             {
-                final ResourceLocation id = ResourceLocation.tryParse(entityString);
+                // TODO: Test with example
+                int nbtStart = entityString.indexOf("{");
+                nbtStart = (nbtStart == -1) ? entityString.length() : nbtStart;
+                String typeString = entityString.substring(0, nbtStart);
+                String nbtString = entityString.substring(nbtStart);
+
+                final ResourceLocation id = ResourceLocation.tryParse(typeString);
                 if (id == null)
                 {
                     DragonfightMod.LOGGER.error("Config entry could not be parsed, not a valid resource location " + entityString);
@@ -40,10 +48,36 @@ public class ConfigurationCache
                     continue;
                 }
 
-                builder.add(type);
+                CompoundTag nbt = null;
+
+                if (!nbtString.isEmpty())
+                {
+                    try
+                    {
+                        nbt = TagParser.parseTag(nbtString);
+                    }
+                    catch (Exception e)
+                    {
+                        DragonfightMod.LOGGER.error("Config entry NBT data could not be parsed for " + entityString, e);
+                    }
+                }
+
+                builder.add(new EntitySpawnData(type, nbt));
             }
         }
 
         return builder.build();
+    }
+
+    public static class EntitySpawnData
+    {
+        public final EntityType  type;
+        public final CompoundTag nbt;
+
+        public EntitySpawnData(final EntityType type, final CompoundTag nbt)
+        {
+            this.type = type;
+            this.nbt = nbt;
+        }
     }
 }
